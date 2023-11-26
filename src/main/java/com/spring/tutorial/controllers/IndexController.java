193d636138f5700f3b133cdafb1819c99aee5669
Yoany Mora Vauxoo <yoany@vauxoo.com>
2023-11-26 16:02:44 -0600

    Show how to pass values from controller to view

--- src/main/java/com/spring/tutorial/controllers/IndexController.java
+++ src/main/java/com/spring/tutorial/controllers/IndexController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
 
 @Controller
 public class IndexController {
 	
 	@GetMapping(value = "/") // la ruta a la que tiene que ir mi navegador, esta ruta solo soporta el verbo GET
-	public String index() {
+	public String index(Model model) {
+		// de esta manera se pueden pasar valores desde el controlador a la vista
+		model.addAttribute("title", "Hola mundo con spring");
+		model.addAttribute("body", "El contenido de Hola mundo con spring");
 		return "indexYeey"; // este es el nombre del archivo HTML que retorna este metodo 
 	}
 	
