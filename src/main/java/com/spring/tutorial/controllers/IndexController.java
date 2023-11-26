a96936f7ef0af73130dbfea3c5eecc761d8102e0
Yoany Mora Vauxoo <yoany@vauxoo.com>
2023-11-26 15:46:16 -0600

    [ADD] create hello world with spring

---
+++ src/main/java/com/spring/tutorial/controllers/IndexController.java
@@ -0,0 +1,22 @@
+package com.spring.tutorial.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping(value = "/") // la ruta a la que tiene que ir mi navegador, esta ruta solo soporta el verbo GET
+	public String index() {
+		return "indexYeey"; // este es el nombre del archivo HTML que retorna este metodo 
+	}
+	
+	/*es buena practica usar el PostMapping o el decorador que tenga el verbo que queremos utilizar pero este es un ejemplo para saber que RequestMApping existe*/
+	@RequestMapping(value="/index-request-mapping", method=RequestMethod.GET) // similar al GetMapping, pero aca se especifica que verbo se utilizara para hacer la peticion http 
+	public String indexRequestMapping() {
+		return "index";
+	}
+	
+}
