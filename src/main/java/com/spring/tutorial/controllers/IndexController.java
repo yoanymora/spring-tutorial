aa97b472e35179bb75aabdeaa967bbd904e2a01b
Yoany Mora Vauxoo <yoany@vauxoo.com>
2023-11-26 17:55:52 -0600

    Each iterator directive with thymeleaf

--- src/main/java/com/spring/tutorial/controllers/IndexController.java
+++ src/main/java/com/spring/tutorial/controllers/IndexController.java
@@ -1,10 +1,12 @@
 package com.spring.tutorial.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.ui.Model;
 
 @Controller
 public class IndexController {
@@ -16,6 +18,9 @@ public class IndexController {
 		model.addAttribute("body", "El contenido de Hola mundo con spring");
 		
 		model.addAttribute("show", true);
+		List<String> ramdomShit = List.of("1", "Casa", "false", "Mikasa", "666");
+		model.addAttribute("ramdomShit", ramdomShit);
+		
 		return "indexYeey"; // este es el nombre del archivo HTML que retorna este metodo 
 	}
 	
