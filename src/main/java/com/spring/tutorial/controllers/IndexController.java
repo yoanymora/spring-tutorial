a2bc622e4a5f9e66f669cfe8f075b7ef0e04174a
Yoany Mora Vauxoo <yoany@vauxoo.com>
2023-11-26 18:07:57 -0600

    Show html with ModelAndView

--- src/main/java/com/spring/tutorial/controllers/IndexController.java
+++ src/main/java/com/spring/tutorial/controllers/IndexController.java
@@ -1,33 +1,26 @@
 package com.spring.tutorial.controllers;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 	
-	@GetMapping(value = "/") // la ruta a la que tiene que ir mi navegador, esta ruta solo soporta el verbo GET
-	public String index(Model model) {
-		// de esta manera se pueden pasar valores desde el controlador a la vista
-		model.addAttribute("title", "Hola mundo con spring");
-		model.addAttribute("body", "El contenido de Hola mundo con spring");
+	@GetMapping(value = "/index-mv")
+	public ModelAndView indexMV(ModelAndView mv) {
+		mv.addObject("title", "Hola mundo con modelAndView");
+		mv.addObject("body", "El contenido de Hola mundo con ModelAndview");
 		
-		model.addAttribute("show", true);
+		mv.addObject("show", true);
 		List<String> ramdomShit = List.of("1", "Casa", "false", "Mikasa", "666");
-		model.addAttribute("ramdomShit", ramdomShit);
-		
-		return "indexYeey"; // este es el nombre del archivo HTML que retorna este metodo 
-	}
-	
-	/*es buena practica usar el PostMapping o el decorador que tenga el verbo que queremos utilizar pero este es un ejemplo para saber que RequestMApping existe*/
-	@RequestMapping(value="/index-request-mapping", method=RequestMethod.GET) // similar al GetMapping, pero aca se especifica que verbo se utilizara para hacer la peticion http 
-	public String indexRequestMapping() {
-		return "index";
+		mv.addObject("ramdomShit", ramdomShit);
+		mv.setViewName("indexYeey");
+		return mv;
 	}
 	
 }
